2ead1d88c00d84e8a492c3965ed98cc5d556923a
Roman Lebedev <lebedev.ri@gmail.com>
2013-09-24 01:36:32 +0400

    Exit menu item now works

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@ int main(int argc, char *argv[])
 {   
     QCoreApplication::setOrganizationName("LRI");
     QCoreApplication::setApplicationName("dbms-v5");
-    QSettings settings;
-
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
 
