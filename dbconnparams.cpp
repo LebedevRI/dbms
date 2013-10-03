49b867258b7c2fcfa7082a4617f629242e042a46
Roman Lebedev <lebedev.ri@gmail.com>
2013-10-03 15:17:01 +0400

    More friendly field names in dbconnparams

--- dbconnparams.cpp
+++ dbconnparams.cpp
@@ -12,11 +12,11 @@ dbconnparams::dbconnparams(QWidget *parent) :
 
     QSettings settings;
     settings.beginGroup("DbConnection");
-    ui->lineEdit->setText(settings.value("HostName", "localhost").toString());
-    ui->lineEdit_2->setText(settings.value("Port", "3306").toString());
-    ui->lineEdit_3->setText(settings.value("UserName", "dbms").toString());
-    ui->lineEdit_4->setText(settings.value("Password", "dbms").toString());
-    ui->lineEdit_5->setText(settings.value("DatabaseName", "dbms").toString());
+    ui->hostName->setText(settings.value("HostName").toString());
+    ui->port->setText(settings.value("Port").toString());
+    ui->userName->setText(settings.value("UserName").toString());
+    ui->password->setText(settings.value("Password").toString());
+    ui->databaseName->setText(settings.value("DatabaseName").toString());
     settings.endGroup();
 }
 
@@ -30,11 +30,11 @@ void dbconnparams::accept()
     QSettings settings;
 
     settings.beginGroup("DbConnection");
-    settings.setValue("HostName", ui->lineEdit->text());
-    settings.setValue("Port", ui->lineEdit_2->text());
-    settings.setValue("UserName", ui->lineEdit_3->text());
-    settings.setValue("Password", ui->lineEdit_4->text());
-    settings.setValue("DatabaseName", ui->lineEdit_5->text());
+    settings.setValue("HostName", ui->hostName->text());
+    settings.setValue("Port", ui->port->text());
+    settings.setValue("UserName", ui->userName->text());
+    settings.setValue("Password", ui->password->text());
+    settings.setValue("DatabaseName", ui->databaseName->text());
     settings.endGroup();
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
